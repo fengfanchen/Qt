d68fd7df40ae92d4f46fc6fcf8a12f498157206d
fengfanchen <78442761@qq.com>
2019-10-04 15:11:51 +0800

    baidu search keyword

---
+++ QtWebSearchKey/widget.cpp
@@ -0,0 +1,52 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QNetworkAccessManager>
+#include <QJsonDocument>
+#include <QMap>
+#include <QVariant>
+#include <QDebug>
+#include <QEventLoop>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("CSDN IT1995");
+    m_manager = new QNetworkAccessManager(this);
+    connect(ui->lineEdit, SIGNAL(textChanged(const QString&)), this, SLOT(inputChanged(const QString&)));
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::inputChanged(const QString &text)
+{
+    QNetworkRequest request;
+    request.setUrl("https://www.baidu.com/sugrec?pre=1&p=3&ie=utf-8&json=1&prod=pc&from=pc_web&sugsid=1430,21123,29523,29721,29567,29220&req=2&csor=5&pwd=123&wd=" + text);
+    QNetworkReply *reply = m_manager->get(request);
+    QEventLoop loop;
+    connect(reply, SIGNAL(finished()), &loop, SLOT(quit()));
+    loop.exec();
+
+    QMap<QString, QVariant> map = QJsonDocument().fromJson(reply->readAll()).toVariant().toMap();
+    //map = map.value("g").toMap();
+    QList<QVariant> list = map.value("g").toList();
+    QStringList widgetList;
+    for(int i = 0; i < list.size(); i++){
+
+        widgetList << list[i].toMap().value("q").toString();
+    }
+
+    resetListWidget(widgetList);
+}
+
+void Widget::resetListWidget(QStringList list)
+{
+    ui->listWidget->clear();
+    ui->listWidget->addItems(list);
+}
---
+++ QtWebSearchKey/widget.h
@@ -0,0 +1,33 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+QT_USE_NAMESPACE
+class QNetworkAccessManager;
+QT_END_NAMESPACE
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+protected slots:
+    void inputChanged(const QString &text);
+
+protected:
+    void resetListWidget(QStringList list);
+
+private:
+    Ui::Widget *ui;
+    QNetworkAccessManager *m_manager;
+};
+
+#endif // WIDGET_H
