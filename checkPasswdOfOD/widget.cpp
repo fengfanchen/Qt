6732c7c02bba285a546dc20fbd237c4102faa683
fengfanchen <78442761@qq.com>
2019-12-05 12:31:17 +0800

    checkPassword about OD

---
+++ checkPasswdOfOD/widget.cpp
@@ -0,0 +1,40 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QMessageBox>
+#include <QDebug>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(btnClicked()));
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::btnClicked()
+{
+    QString passwd = ui->lineEdit->text();
+
+    if(isOK(passwd)){
+
+
+        QMessageBox::information(this, "提示", "成功");
+    }
+    else{
+
+        QMessageBox::information(this, "提示", "失败");
+    }
+}
+
+bool Widget::isOK(QString str)
+{
+    if(str == "www.it1995.cn")
+        return true;
+
+    return false;
+}
---
+++ checkPasswdOfOD/widget.h
@@ -0,0 +1,28 @@
+﻿#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = nullptr);
+    ~Widget();
+
+protected slots:
+    void btnClicked();
+
+protected:
+    bool isOK(QString str);
+
+private:
+    Ui::Widget *ui;
+};
+
+#endif // WIDGET_H
